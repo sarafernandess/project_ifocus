1ce4d3826d2b0ab628fccba82fda6d0871de4361
sarafernandes506 <dmhsouza@outlook.com>
2025-10-13 15:27:53 -0300

    feat: rotas api

---
+++ backend/app/api/router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from app.routes.course_routes import router as course_router
+from app.routes.discipline_routes import router as discipline_router
+from app.routes.user_routes import router as user_router, helpers_router, public_router
+from app.routes.chat_routes import router as chat_router
+# from app.routes.user_routes import router as user_router (exemplo futuro)
+
+api_router = APIRouter()
+
+# Adicione suas rotas aqui
+api_router.include_router(user_router)
+api_router.include_router(course_router)
+api_router.include_router(discipline_router)
+api_router.include_router(chat_router, prefix="")
+api_router.include_router(helpers_router)
+api_router.include_router(public_router)
+# api_router.include_router(user_router)
